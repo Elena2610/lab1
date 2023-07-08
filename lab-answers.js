4e5fadc7df9c7be4c8c7eba182163d78b1ee84ed
Elena <vega22.Elena@gmail.com>
2023-07-07 22:50:47 -0500

    Where is Waldo answered

--- lab-answers.js
+++ lab-answers.js
@@ -77,3 +77,11 @@ if (g%2==0){k=g/2-1}
     } 
   }
   
+  const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                      ["Lucinda", "Jacc", "Neff", "Snoop"],
+                      ["Petunia", ["Baked Goods", "Waldo"]]];
+                      whereIsWaldo.splice(1,1);
+                      console.log(whereIsWaldo);
+                      whereIsWaldo[1][2]="No One";
+                      console.log(whereIsWaldo);
+                      console.log(whereIsWaldo[2][1][1]);
